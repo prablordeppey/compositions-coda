b6e57311fcb6fd59c5804488e7d157480eb2e008
icetee4eva <30740369+prablordeppey@users.noreply.github.com>
2022-04-20 20:11:24 +0100

    GH action update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="compositions_coda-prablordeppey",
+    name="compositions_coda -prablordeppey",
     version="0.0.1",
     author="Ablordeppey Prosper",
     author_email="prablordeppey@gmail.com",
